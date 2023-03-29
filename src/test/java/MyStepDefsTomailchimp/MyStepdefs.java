bc6abc0652f049ffd57969c50c3a618a694e3329
githublin8888 <chunlinzhou2017@hotmail.com>
2023-03-29 23:48:03 +0200

    test occupiedName

--- src/test/java/MyStepDefsTomailchimp/MyStepdefs.java
+++ src/test/java/MyStepDefsTomailchimp/MyStepdefs.java
@@ -19,9 +19,10 @@ import static org.junit.Assert.assertEquals;
 public class MyStepdefs {
     private WebDriver driver;
     private WebDriverWait wait;
+  //  String name = "";
 
     @Given("I have chosen a {string} and logged in Mailchimp")
-    public void iHaveChosenAandLoggedinMailchimp(String browser) throws InterruptedException{
+    public void iHaveChosenAandLoggedinMailchimp(String browser) throws InterruptedException {
 
 
         if (browser.equalsIgnoreCase("chrome")) {
@@ -34,12 +35,12 @@ public class MyStepdefs {
             driver.manage().window().maximize();
 
             WebElement element = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("onetrust-reject-all-handler")));
-          element.click();
+            element.click(); //cookies
 
             //driver.findElement(By.id("onetrust-reject-all-handler")).click();//cookies
 
-           // wait.until(ExpectedConditions.presenceOfElementLocated(By.id("onetrust-reject-all-handler")));
-           // driver.findElement(By.id("onetrust-reject-all-handler")).click();
+            // wait.until(ExpectedConditions.presenceOfElementLocated(By.id("onetrust-reject-all-handler")));
+            // driver.findElement(By.id("onetrust-reject-all-handler")).click();
 
         } else if (browser.equalsIgnoreCase("edge")) {
             System.setProperty("webdriver.edge.driver", "C:\\Selenium\\msedgedriver.exe");
@@ -48,20 +49,26 @@ public class MyStepdefs {
             driver.manage().window().maximize();
             WebElement element = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("onetrust-reject-all-handler")));
             element.click();
-
+            Thread.sleep(2000);
         }
-        wait = new WebDriverWait(driver, 10);
 
+        wait = new WebDriverWait(driver, 20);
     }
 
     @Given("I enter my {string}")
-    public void iEnterMy(String email) {
+    public void iEnterMy(String email) throws InterruptedException {
         driver.findElement(By.id("email")).sendKeys(email);
+        Thread.sleep(3000);
     }
 
     @And("I enter a {string}")
     public void iEnterA(String username) {
-        driver.findElement(By.name("username")).sendKeys(username);
+
+        WebElement field = driver.findElement(By.name("username"));
+        field.click();
+        field.clear();
+        field.sendKeys(username);
+       // name = driver.findElement(By.name("username")).getText();
     }
 
     @And("I select a {string}")
@@ -77,37 +84,39 @@ public class MyStepdefs {
     }
 
 
-
     @Then("My registration will be {string}")
     public void myRegistrationWillBe(String result) {
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"signup-content\"]/div/div[1]/section/div/h1")));
-        if (result.equals("yes")) {
-
-            String actual = driver.findElement(By.xpath("//*[@id=\"signup-content\"]/div/div[1]/section/div/h1")).getText();
-            String expected = "Check your email";
-            assertEquals(expected, actual);
-        }else if(result.equals("tooLong")){
-            String actual = driver.findElement(By.className("invalid-error")).getText();
+       // wait.until(ExpectedConditions.presenceOfElementLocated(By.className("!margin-bottom--lv3")));
+        if (result.equals("tooLong")) {
+            String actual = driver.findElement(By.className("invalid-error")).getText();//#signup-form > fieldset > div:nth-child(2) > div > span
             String expected = "Enter a value less than 100 characters long";
             assertEquals(expected, actual);
-        } else if (result.equals("occupiedName")) {
-            String actual = driver.findElement(By.cssSelector("#signup-form > fieldset > div:nth-child(2) > div > span")).getText();
-            String expected = "Enter a value less than 100 characters long";  //By.cssSelector(".invalid-error")
+        } else if (result.equals("occupiedName")) {//By.cssSelector("#signup-form > fieldset > div:nth-child(2) > div > span")
+            // String name=driver.findElement(By.name("username")).getText();
+            //String locator = "a[href=\"/?username=" + name + "\"]";  //a[href="/?username=chunlin"]  "a[href=\"/?username="+name+"\"]"
+          //  String actual = driver.findElement(By.cssSelector(locator)).getText();
+            String actual = driver.findElement(By.cssSelector("a[href$='username=']")).getText();
+            String expected = "log in";  //By.cssSelector(".invalid-error")
             assertEquals(expected, actual);
-        }else if (result.equals("noEmail")) {
+        } else if (result.equals("noEmail")) {
             String actual = driver.findElement(By.cssSelector("#signup-form > fieldset > div:nth-child(1) > div > span")).getText();
             String expected = "An email address must contain a single @.";
             assertEquals(expected, actual);
+        } else if (result.equals("yes")) {
+
+            String actual = driver.findElement(By.className("!margin-bottom--lv3")).getText();
+            String expected = "Check your email";
+            assertEquals(expected, actual);
         }
 
-//By.cssSelector(".\\!margin-bottom--lv3")
+
     }
+
     @After
     public void tearDown() {
         driver.close();
         driver.quit();
     }
-
 
 
 }
