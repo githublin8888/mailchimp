0f541137e07b206ef3c267d1780857995f6ae50e
githublin8888 <chunlinzhou2017@hotmail.com>
2023-08-10 22:49:56 +0200

    add uppgift beskrivning

---
+++ src/test/java/MyStepDefsTomailchimp/UppgiftBeskrivning.java
@@ -0,0 +1,34 @@
+package MyStepDefsTomailchimp;
+
+/*
+
+Registrera användare
+
+På https://login.mailchimp.com/signup/ kan man registrera sig som användare
+
+Skriv testfall som testar denna registreringsfunktion genom automatisering via Cucumber och Selenium
+
+Testfallen skall täcka 2 krav:
+
+1: Scenarion
+
+Skapa användare – allt går som förväntat
+
+Skapa användare – långt användarnamn (mer än 100 tecken)
+
+Skapa användare – användare redan upptagen
+
+Skapa användare – e-mail saknas
+
+2: Krav på testfall
+
+Testfallen skall gå att köra igen utan att behöva förändra koden i testfallen och så att testfallen fortfarande ger rätt resultat
+
+Testfallen skall skapas genom en och enbart en Scenario Outline ifrån en feature-fil
+
+Testfallen skall köras på minst två browsrar
+
+Skapa minst en privat metod som använder sig av explicit wait
+
+
+ */
